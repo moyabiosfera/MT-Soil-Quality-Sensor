05f257ba58b552b13c4ce8e37edb32c45dfed1a5
Dani Rguez Moya <119592616+99danirmoya@users.noreply.github.com>
2025-05-26 20:44:19 +0200

    added freertos version

---
+++ soil_quality_sensor_freertos/.pio/build/esp32dev/lib3eb/WebServer/detail/mimetable.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32dev/lib3eb/WebServer/detail/mimetable.cpp.o: \
+ C:/Users/danie/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.cpp \
+ C:/Users/danie/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.h \
+ C:/Users/danie/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
