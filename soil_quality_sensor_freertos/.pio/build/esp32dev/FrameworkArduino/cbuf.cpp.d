7a0e6d169fbfefa0230e56cc02bdb8b6dd0f04a0
Dani Rguez Moya <119592616+99danirmoya@users.noreply.github.com>
2025-05-26 20:45:36 +0200

    Delete soil_quality_sensor_freertos directory

--- soil_quality_sensor_freertos/.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.o: \
- C:/Users/danie/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- C:/Users/danie/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
